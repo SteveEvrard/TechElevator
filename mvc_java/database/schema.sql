537efbfa8853a13e6bd51b2349e6960d2a8fc833
Eric Cardenas <bojangles010@gmail.com>
2019-08-06 11:36:26 -0400

    created tables

--- mvc_java/database/schema.sql
+++ mvc_java/database/schema.sql
@@ -15,4 +15,25 @@ CREATE TABLE app_user (
   salt varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS event;
+
+CREATE TABLE event
+(
+event_id SERIAL PRIMARY KEY,
+date_time    TIMESTAMP,
+location VARCHAR(255),
+title varchar(255),
+has_occured boolean,
+is_private boolean
+);
+
+DROP TABLE IF EXISTS joinTable;
+
+CREATE TABLE joinTable
+(
+event_id serial REFERENCES     event(event_id),
+id SERIAL REFERENCES users(id)
+)
+;
+
 COMMIT;
