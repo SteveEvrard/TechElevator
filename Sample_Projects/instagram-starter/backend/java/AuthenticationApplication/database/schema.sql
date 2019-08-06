91fc44c6d4e543369d42169f03185e09bc2dfdbe
Eric Cardenas <bojangles010@gmail.com>
2019-08-06 11:40:07 -0400

    tables again

--- Sample_Projects/instagram-starter/backend/java/AuthenticationApplication/database/schema.sql
+++ Sample_Projects/instagram-starter/backend/java/AuthenticationApplication/database/schema.sql
@@ -11,6 +11,25 @@ CREATE TABLE users (
   image varchar(200)
 );
 
+DROP TABLE IF EXISTS event;
 
+CREATE TABLE event
+(
+event_id SERIAL PRIMARY KEY,
+date_time    TIMESTAMP,
+location VARCHAR(255),
+title varchar(255),
+has_occured boolean,
+is_private boolean
+);
+
+DROP TABLE IF EXISTS joinTable;
+
+CREATE TABLE joinTable
+(
+event_id serial REFERENCES     event(event_id),
+id SERIAL REFERENCES users(id)
+)
+;
 
 COMMIT TRANSACTION;
