7b65fba60af6a99c76c225dcdf6aebf2732fe792
Zach <zachtrembly@gmail.com>
2019-08-07 12:12:55 -0400

    pulling new tables

--- vue_starter_java/backend/src/test/java/com/techelevator/tests/JdbcWhiskeydao.java
+++ vue_starter_java/backend/src/test/java/com/techelevator/tests/JdbcWhiskeydao.java
@@ -1,13 +1,26 @@
 package com.techelevator.tests;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
 import com.techelevator.model.Whiskey;
 
+@Component	
 public class JdbcWhiskeyDao implements WhiskeyDao {
+	
+	private JdbcTemplate jdbcTemplate;
 
-	@Override
+	@Autowired public JdbcWhiskeyDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 	public void saveWhiskey(Whiskey whiskey) {
-		// TODO Auto-generated method stub
-		
+		String sql = "INSERT INTO whiskey(brand), whiskey(price) " +
+				"VALUES (DEFAULT, ?);";
+		jdbcTemplate.update(sql,whiskey.getPrice(), whiskey.getBrand());
 	}
 
 }
