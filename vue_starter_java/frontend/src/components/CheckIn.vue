38adbb0b255e829c689ad4c53095c476a63ba870
Eric Cardenas <ecardenas@Techs-MacBook-Air-5.local>
2019-08-07 14:29:07 -0400

    safety commit

---
+++ vue_starter_java/frontend/src/components/CheckIn.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="checkIn" class="text-center">
+<form 
+    </div>    
+</template>
+
+<script>
+
+import SingleEvent from "../components/SingleEvent.vue";
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
