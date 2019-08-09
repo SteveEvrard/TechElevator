13cafc88010baa41cef99f25cf07fffdaef83478
Beth <1bethford@gmail.com>
2019-08-09 09:35:22 -0400

    Whiskey API post start

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/WhiskeyController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/WhiskeyController.java
@@ -2,11 +2,17 @@ package com.techelevator.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
+import com.techelevator.model.Event;
 import com.techelevator.model.Whiskey;
 import com.techelevator.model.WhiskeyDao;
 
@@ -25,5 +31,17 @@ public class WhiskeyController {
 		public List<Whiskey> getWhiskeyBrands() {
 			return whiskeyDao.getAllBrandAndPriceWhiskey();
 		}
+		
+//		@PostMapping(path="/api/whiskeys")
+//		public ResponseEntity<Event> createEvent(@RequestBody List<Whiskey> whiskeyBrands) {
+//			for(Whiskey placeholder : whiskeyBrands) {
+//				whiskeyDao.saveWhiskey(placeholder);
+//
+//			UriComponents uriComponent = ServletUriComponentsBuilder.fromCurrentRequestUri()
+//			.path("/" + placeholder.getWhiskeyId()).build();
+//			
+//			return ResponseEntity.created(uriComponent.toUri()).body(placeholder);
+//			}
+//		}
 	
 }
