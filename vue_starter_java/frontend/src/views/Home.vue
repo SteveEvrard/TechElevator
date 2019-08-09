fc44359c0dd5dab33916f739bd2648b6426ff43a
Beth <1bethford@gmail.com>
2019-08-09 10:27:45 -0400

    Vue Styling Home page 60% done

--- vue_starter_java/frontend/src/views/Home.vue
+++ vue_starter_java/frontend/src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <div class="home">
-      <img src="@/assets/img/TKWS_Black Logo.png">
-    </div>
+  <div class="home">
     <div class="list-of-events">
       <event-list :apiURL="API_URL"></event-list>
       <check-in></check-in>
@@ -27,15 +24,12 @@ export default {
 </script>
 
 <style>
-.home img {
-  max-width: 800px;
-}
 .list-of-events {
   background-color: #2e4d58;
   padding: 1px;
   width: 80%;
   height: fit-content;
-  border-radius: 5px;
+  border-radius: 10px;
   margin: 5%;
   box-shadow: -2px 10px 18px -4px rgba(0, 0, 0, 0.75);
 }
