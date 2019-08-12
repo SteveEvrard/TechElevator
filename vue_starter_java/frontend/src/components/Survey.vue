db6b432bd6182584c22faa75606a0d65b305f7e9
Eric Cardenas <ecardenas@Techs-MacBook-Air-5.local>
2019-08-12 08:41:57 -0400

    survey started

---
+++ vue_starter_java/frontend/src/components/Survey.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="survey">
+        <h1 class="whiskeyName">
+            {{title}}
+        </h1>
+<div class="questions">
+<single-whiskey-rating></single-whiskey-rating>
+</div>
+    </div>
+</template>
+
+<script>
+import SingleEvent from "../components/SingleEvent";
+import SingleWhiskeyRating from "../components/SingleWhiskeyRating";
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
