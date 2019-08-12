f7d4c24e31f5d141e2832e30351d7732b7a64128
Steve <stevelevrard@gmail.com>
2019-08-12 15:13:22 -0400

    added method to get ratings by event and whiskey

--- vue_starter_java/backend/src/main/java/com/techelevator/model/WhiskeyRatingDao.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/model/WhiskeyRatingDao.java
@@ -7,6 +7,7 @@ public interface WhiskeyRatingDao {
 
 	List<WhiskeyRating> getRatingsByEvent(long eventId);
 	List<WhiskeyRating> getRatingsByUser(long userId);
+	List<WhiskeyRating> getRatingsByEventAndWhiskey(long eventId, long userId);
 	void submitRating(WhiskeyRating rating);
 
 }
